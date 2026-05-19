cac8ef6d2e8032367acc1293c70483841d5d2dc9
sebastian <zsebastian43212@gmail.com>
2026-05-18 22:43:09 -0400

    Update myApp.js
    Signed-off-by: sebastian <zsebastian43212@gmail.com>

--- myApp.js
+++ myApp.js
@@ -5,6 +5,12 @@ console.log("Hello World");
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
